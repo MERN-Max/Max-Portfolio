4a5969e750e8feb33c45e37e155aa9687b5f1dd9
MERN-Max <munenemaxwell2@gmail.com>
2021-07-22 21:16:57 +0300

    edit skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,7 @@ sr.reveal('.about_text', { delay: 400 });
 sr.reveal('.skills_subtitle', {});
 sr.reveal('.skills_text', { delay: 200 });
 sr.reveal('.skills_data', { delay: 200 });
-sr.reveal('.skill_item', { interval: 200 });
+sr.reveal('.skill_item', { interval: 100 });
 sr.reveal('.skills_img', { delay: 400 });
 
 // Scroll work
